dc2d2b41f92a6380fe22d295351d903aca603434
Jil Sahm <j.sahm@gmx.de>
2019-12-24 11:37:10 +0100

    command parsing (in progress)

--- src/command.rs
+++ src/command.rs
@@ -1,12 +1,15 @@
 use std::{
+    str::FromStr,
     time::Duration,
     thread::sleep,
 };
 
+use regex::Regex;
 use crate::keyboard::{Key, KeyBoard};
 
-pub trait Command {
+pub trait Command: FromStr {
     fn trigger(&self, keyboard: &KeyBoard);
+    fn pattern() -> &'static Regex;
 }
 
 pub struct Move {
@@ -20,6 +23,25 @@ impl Command for Move {
         keyboard.key_down(self.direction.clone());
         sleep(Duration::from_millis(self.range));
         keyboard.key_up(self.direction.clone());
+    }
+
+    fn pattern() -> &'static Regex {
+        lazy_static! {
+            static ref pattern: Regex = "^m[rl][0-9]{1-4}$".parse().expect("Check move regex");            
+        } 
+        &pattern       
+    }
+}
+
+impl FromStr for Move {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if Move::pattern().is_match(s) {
+            //TODO
+        } else {
+            Err("".into())
+        }
     }
 }
 
